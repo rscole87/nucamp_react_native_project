ba4d734b7003fad747647acef418451853f3fb2a
Rodrick Coleman <rscole87@gmail.com>
2021-08-10 21:32:59 -0400

    add local notifications and email sharing

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet, Picker, Switch, Button, Alert, PanR
 import DateTimePicker from "@react-native-community/datetimepicker";
 import { colors } from "react-native-elements";
 import * as Animatable from "react-native-animatable";
+import * as Notifications from 'expo-notifications'
 
 class Reservation extends Component {
   constructor(props) {
@@ -34,7 +35,10 @@ class Reservation extends Component {
         },
         {
           text: 'OK',
-          onPress: () => this.resetForm()
+          onPress: () => { 
+            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+            this.resetForm();
+          }
         }
       ],
       {cancelable: false}
@@ -48,6 +52,32 @@ class Reservation extends Component {
       date: new Date(),
       showCalendar: false,
     });
+  }
+
+  async presentLocalNotification(date) {
+    function sendNotification(){
+      Notifications.setNotificationHandler({
+        handleNotification: async () => ({
+          shouldShowAlert: true
+        })
+      })
+
+      Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'Your Campsite Reservation Search',
+          body: `Search for ${date} requested`
+        },
+        trigger: null
+      });
+    }
+
+    let permissions = await Notifications.getPermissionsAsync();
+    if(!permissions.granted) {
+      permissions = await Notifications.requestPermissionsAsync();
+    }
+    if(permissions.granted){
+      sendNotification();
+    }
   }
 
   render() {
